f317a99c48f8d7c7109299d88b8e661be6aa488b
Jun Kyung Lee <junlee0325@gmail.com>
2026-05-23 02:23:06 -0400

    Fix dependencies and sync lockfile for EAS

--- components/haptic-tab.tsx
+++ components/haptic-tab.tsx
@@ -1,13 +1,31 @@
-import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
-import { PlatformPressable } from '@react-navigation/elements';
-import * as Haptics from 'expo-haptics';
+// import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
+// import { PlatformPressable } from '@react-navigation/elements';
+// import * as Haptics from 'expo-haptics';
 
-export function HapticTab(props: BottomTabBarButtonProps) {
+// export function HapticTab(props: BottomTabBarButtonProps) {
+//   return (
+//     <PlatformPressable
+//       {...props}
+//       onPressIn={(ev) => {
+//         if (process.env.EXPO_OS === 'ios') {
+//           // Add a soft haptic feedback when pressing down on the tabs.
+//           Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+//         }
+//         props.onPressIn?.(ev);
+//       }}
+//     />
+//   );
+// }
+
+import * as Haptics from "expo-haptics";
+import { Pressable, type PressableProps } from "react-native";
+
+export function HapticTab(props: PressableProps) {
   return (
-    <PlatformPressable
+    <Pressable
       {...props}
       onPressIn={(ev) => {
-        if (process.env.EXPO_OS === 'ios') {
+        if (process.env.EXPO_OS === "ios") {
           // Add a soft haptic feedback when pressing down on the tabs.
           Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
         }
